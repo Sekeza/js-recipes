848f125733be92333f51b89aacc64e7a754de567
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-03 14:29:47 +0100

    Adding an empty array variable, then a function to make requests with a named search parameter from input

---
+++ js/index.js
@@ -0,0 +1,94 @@
+
+console.log("working");
+// Selecting the elements
+const image = document.querySelector(".trends .box-container .box img");
+const h5 = document.querySelector(".trends .box-container .box h5");
+
+const fruitImage = document.querySelector(".fruits .box-container .box img");
+const fruitName = document.querySelector(".fruits .box-container .box h5");
+
+const form = document.querySelector("form");
+const inputSearch = document.querySelector(".form #input");
+
+form.addEventListener("submit", () => {
+    inputSearch.addEventListener("blur/change/", () => {
+
+    });
+});
+
+
+
+
+
+const trendsList = [];
+
+// Get data from the api for trends
+const getTrends = async() => {
+    const check = localStorage.getItem("trends");
+    if(check) {
+        JSON.parse(check);
+    }
+    else {
+        const api = await fetch(`https://api.spoonacula.com/recipes/random?apikey=${process.env.RECIPE_API_KEY}&number=10`);
+
+        const data1 = api.json();
+
+        localStorage.setItem("fruits", JSON.stringify(data1));
+        console.log(data1.recipes);
+        trendsList.push(...data1);
+
+    }
+    
+
+    trendsList.map(recipe => {
+        return {
+           image: recipe.image, 
+           h5: recipe.title
+        }
+    });
+};
+
+getTrends();
+
+
+const fruitsList = [];
+
+// Get data from the api for trends
+const getFruits = async() => {
+    const check = localStorage.getItem("fruits");
+    if(check) {
+        JSON.parse(check);
+    }
+    else {
+        const api = await fetch(`https://api.spoonacula.com/recipes/random?apikey=${process.env.RECIPE_API_KEY}&number=10&tag=fruits`);
+
+        const data1 = api.json();
+
+        localStorage.setItem("fruits", JSON.stringify(data1));
+        console.log(data1.recipes);
+        fruitsList.push(...data1);
+    }
+
+    
+    fruitsList.map(recipe => {
+        return {
+           fruitImage: recipe.image, 
+           fruitName: recipe.title
+        }
+    });
+};
+
+getFruits();
+
+const searchList = [];
+
+const getSearch = async(searchName) => {
+    const api = await fetch(`https://api.spoonacula.com/recipes/complexSearch?apikey=${process.env.RECIPE_API_KEY}&number=10&name=${searchName}`);
+
+    const searchData = api.json();
+
+    console.log(searchData.recipes);
+    searchList.push(...searchData);
+}
+
+
