cf368a35acfb5812a4ce0c652f3e9a8c2fd65c97
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-05 15:44:46 +0100

    Copied the api options and fetch method from spoonacular api

--- js/index.js
+++ js/index.js
@@ -12,6 +12,19 @@ const inputSearch = document.querySelector(".form #input").value;
 
 const boxContainer = document.querySelector(".fruits .box-container .box img");
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '31df0a0b32mshbbe49ba067f1ad2p19236fjsn74d4a66078c8',
+		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+	}
+};
+
+fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian%2Cdessert&number=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
 form.addEventListener("submit", () => {
     inputSearch.addEventListener("blur/change/", () => {
 
