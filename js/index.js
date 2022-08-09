eaaef20ee30386a7ac26948b12de300c1c43019c
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-09 15:34:39 +0100

    Re-wrote the getTrends function from the start

--- js/index.js
+++ js/index.js
@@ -1,34 +1,31 @@
 console.log("working well");
 
 const trendsContainer = document.querySelector(".trends .box-container");
-console.log(trendsContainer);
-let trendsList = [];
-console.log(trendsList);
 
 // Get data from the api for trends
 const getTrends = async () => {
    
-    let api = await fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10");
+    let url = "https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10";
    
-    let data1 = await api.json();
-    console.log(data1.recipes);
-    trendsList.push(...data1.recipes);
-    console.log(trendsList);
-            
+    try {
+        let apiData1 = await fetch(url);
+        return apiData1.json();
+    } catch (error) {
+        console.log(error);
+    }            
 };
 
 const displayTrendingRecipes =  () => {
-    const trendRecipes = getTrends();
+    let trendsList = getTrends();
 
     let html = "";
-    trendRecipes.map(trend => {
+    trendsList.forEach(trend => {
         let divCard = `
             <div class="box">
                 <img src={trend.image} alt={trend.image}>
                 <h5>{trend.title}</h5>
             </div>
         `;
-
         html += divCard;
     });
     trendsContainer.innerHTML = html;
