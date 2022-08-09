cfe22610a6af9c3dbe5b243a98365f6f412fea4f
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-09 14:31:15 +0100

    Editing the getTrends function and created the display function for its data

--- js/index.js
+++ js/index.js
@@ -1,118 +1,40 @@
-// import { RECIPE_API_KEY } from "../";
+console.log("working well");
 
-console.log("working");
-// Selecting the elements
-const image = document.querySelector(".trends .box-container .box img");
-const h5 = document.querySelector(".trends .box-container .box h5");
-
-const fruitImage = document.querySelector(".fruits .box-container .box img");
-const fruitName = document.querySelector(".fruits .box-container .box h5");
-
-const form = document.querySelector("form");
-// const inputSearch = document.querySelector(".form #input").value;
-
-const boxContainer = document.querySelector(".fruits .box-container .box img");
-
-const apiKey = config.RECIPE_API_KEY;
-// form.addEventListener("submit", () => {
-//     inputSearch.addEventListener("blur/change/", () => {
-
-//     });
-// });
-
-
-
-
-
-
-const trendsList = [];
+const trendsContainer = document.querySelector(".trends .box-container");
+console.log(trendsContainer);
+let trendsList = [];
+console.log(typeof trendsList);
 
 // Get data from the api for trends
-const getTrends = async() => {
-    const check = localStorage.getItem("trends");
-    if(check) {
-        JSON.parse(check);
-    }
-    else {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=10`, { method:"GET", headers: { "Content-Type": "application/json" }});
-        // const api = await fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10", { method:"GET", headers: { "Content-Type": "application/json" }});
+const getTrends = async () => {
+   
+    const api = await fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10", { method:"GET", headers: { "Content-Type": "application/json" }});
 
-        const data1 = api.json();
-
-        localStorage.setItem("fruits", JSON.stringify(data1));
-        console.log(data1);
-        // trendsList.push(...data1);
-
-    }
-    
-
-    // trendsList.map(recipe => {
-    //     return {
-    //        image: recipe.image, 
-    //        h5: recipe.title
-    //     }
-    // });
+    const data1 = api.json();
+    console.log(data1);
+    // trendsList.push(...data1);
+            
 };
 
-getTrends();
+const displayTrendingRecipes = async () => {
+    const trendRecipes = getTrends();
+
+    let html = "";
+    trendRecipes.forEach(trend => {
+        let divCard = `
+            <div class="box">
+                <img src={trend.image} alt={trend.image}>
+                <h5>{trend.title}</h5>
+            </div>
+        `;
+
+        html += divCard;
+    })
+
+}
+
+trendsContainer.innerHTML = html;
 
 
-// const fruitsList = [];
-
-// // Get data from the api for trends
-// const getFruits = async() => {
-//     const check = localStorage.getItem("fruits");
-//     if(check) {
-//         JSON.parse(check);
-//     }
-//     else {
-//         const api = await fetch(`https://api.spoonacula.com/recipes/random?apikey=${process.env.RECIPE_API_KEY}&number=10&tag=fruits`);
-
-//         const data1 = api.json();
-
-//         localStorage.setItem("fruits", JSON.stringify(data1));
-//         console.log(data1.recipes);
-//         fruitsList.push(...data1);
-//     }
-
-    
-//     fruitsList.map(recipe => {
-//         return {
-//            fruitImage: recipe.image, 
-//            fruitName: recipe.title
-//         }
-//     });
-// };
-
-// getFruits();
-
-// const searchList = [];
-
-// const getSearch = async(searchName) => {
-//     const api = await fetch(`https://api.spoonacula.com/recipes/complexSearch?apikey=${process.env.RECIPE_API_KEY}&number=10&name=${searchName}`);
-
-//     const searchData = api.json();
-
-//     console.log(searchData.recipes);
-//     searchList.push(...searchData);
-// }
-
-// const findMatches = (searchItem, searchList) => {
-//     searchList.filter(recipe => {
-//         return recipe.name.toLowerCase().includes(searchItem.toLowerCase());
-//     });
-// }
-
-// const displaySearch = (inputSearch) => {
-//     const matchList = findMatches(inputSearch, searchList);
-//     const cardHtml = matchList.map(item => {
-//         return `
-//             <div class="box">
-//                 <img src={item.image} alt={item.name}>
-//                 <h5>{item.name}</h5>
-//             </div>
-//         `
-//     }).join("");
-// }
 
 
