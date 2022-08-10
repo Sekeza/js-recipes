6576ed1b38f9d6ddf5e80e3df6b173f97c98c411
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-10 08:58:17 +0100

    Finalized the api request for trending recipes & display same on the user interface

--- js/index.js
+++ js/index.js
@@ -2,51 +2,29 @@ console.log("working well");
 
 const trendsContainer = document.querySelector(".trends .box-container");
 
+// Get the trending recipes from API 
 fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10")
     .then((data) => {
         return data.json();
     })
     .then((result) => {
         console.log(result.recipes);
-        let html = "";
+        let trendCard = "";
         result.recipes.map((item) => {
-            html += `
+            trendCard += `
             <div class="box">
                 <img src=${item.image} alt=${item.title} >
                 <h5>${item.title}</h5>
             </div>
         `;
         });
-        trendsContainer.innerHTML = html; 
+        trendsContainer.innerHTML = trendCard; 
     })
     .catch ((error) => {
         console.log(error);
     })
 
-// Get data from the api for trends
-// const getTrends = async () => {
-   
-//     let url = "https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10";
-   
-//     try {
-//         let apiData1 = await fetch(url);
-//         // console.log( apiData1.json());
-//         return await apiData1.json();
-//     } catch (error) {
-//         console.log(error);
-//     }            
-// };
 
-// // getTrends();
-
-// const displayTrendingRecipes = async () => {
-//     let trendsList = await getTrends();
-//     console.log(trendsList);
-
-// 
-
-
-// displayTrendingRecipes();
 
 
 
