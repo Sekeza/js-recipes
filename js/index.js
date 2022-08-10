c8708f4900d8ab000394ecf62e06e77074b1b277
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-10 09:11:33 +0100

    Added the fetch method to get the fruits recipes from the API & display same in the browser

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const trendsContainer = document.querySelector(".trends .box-container");
 const fruitsContainer = document.querySelector(".fruits .box-container");
 
 // Get the trending recipes from API 
-fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=10")
+fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&number=4")
     .then((data) => {
         return data.json();
     })
@@ -24,6 +24,29 @@ fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee8
     .catch ((error) => {
         console.log(error);
     })
+
+
+// Get the fruit recipes from API 
+fetch("https://api.spoonacular.com/recipes/random?apiKey=0685a694f3f54a7ea76bee81e370bd17&&tag=fruit&number=4")
+    .then((data1) => {
+        return data1.json();
+    })
+    .then((result) => {
+        console.log(result.recipes);
+        let fruitCard = "";
+        result.recipes.map((item) => {
+            fruitCard += `
+            <div class="box">
+                <img src=${item.image} alt=${item.title} >
+                <h5>${item.title}</h5>
+            </div>
+        `;
+        });
+        trendsContainer.innerHTML = fruitCard; 
+    })
+    .catch ((error) => {
+        console.log(error);
+    })    
 
 
 
